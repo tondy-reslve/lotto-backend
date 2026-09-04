b601e0e4a995abd67019857d0c6542115149210d
Tondy <tondyztd@163.com>
2026-09-04 15:57:59 +0800

    Refactor JwtDecoder bean to use HttpClient for SSL context configuration

--- src/main/java/com/nlnl/lotto/config/SecurityConfig.java
+++ src/main/java/com/nlnl/lotto/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package com.nlnl.lotto.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ssl.SslBundles;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.net.http.HttpClient;
 
 @Configuration
 public class SecurityConfig {
@@ -16,12 +19,17 @@ public class SecurityConfig {
     private String issuerUri;
 
     @Bean
-    public JwtDecoder jwtDecoder(RestTemplateBuilder builder, SslBundles sslBundles) {
-        RestTemplate restTemplate = builder
-                .setSslBundle(sslBundles.getBundle("keycloak-bundle"))
+    public JwtDecoder jwtDecoder(SslBundles sslBundles) {
+        SSLContext sslContext = sslBundles.getBundle("keycloak-bundle").createSslContext();
+        
+        HttpClient httpClient = HttpClient.newBuilder()
+                .sslContext(sslContext)
                 .build();
+        
+        RestTemplate restTemplate = new RestTemplate(new JdkClientHttpRequestFactory(httpClient));
+        
         String jwkSetUri = issuerUri + "/protocol/openid-connect/certs";
-
+        
         return NimbusJwtDecoder.withJwkSetUri(jwkSetUri)
                 .restOperations(restTemplate)
                 .build();
