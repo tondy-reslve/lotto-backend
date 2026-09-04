7641065cd65fc0fdbdcd46f3677a33bfefb88f7c
Tondy <tondyztd@163.com>
2026-09-04 15:26:17 +0800

    Refactor JwtDecoder configuration to streamline issuer URI handling

--- src/main/java/com/nlnl/lotto/config/SecurityConfig.java
+++ src/main/java/com/nlnl/lotto/config/SecurityConfig.java
@@ -12,19 +12,17 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class SecurityConfig {
 
-    // 讀取 application.yml 裡的 issuer-uri
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuerUri;
 
     @Bean
     public JwtDecoder jwtDecoder(RestTemplateBuilder builder, SslBundles sslBundles) {
-        // 1. 讓 RestTemplate 吃下你在 application.yml 定義的 keycloak-bundle
         RestTemplate restTemplate = builder
                 .setSslBundle(sslBundles.getBundle("keycloak-bundle"))
                 .build();
-        
-        // 2. 讓 JwtDecoder 透過這個帶有 K8s-Local-CA 的 RestTemplate 去連 Keycloak
-        return NimbusJwtDecoder.withIssuerLocation(issuerUri)
+        String jwkSetUri = issuerUri + "/protocol/openid-connect/certs";
+
+        return NimbusJwtDecoder.withIssuerLocation(jwkSetUri)
                 .restOperations(restTemplate)
                 .build();
     }
